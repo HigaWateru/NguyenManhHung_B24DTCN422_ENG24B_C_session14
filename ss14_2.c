fdea04d5862f7d0f925bda38a417edb951f45d1f
Nguyễn Mạnh Hùng <106744156+HigaWateru@users.noreply.github.com>
2024-12-11 20:16:15 +0700

    Delete ss14_2.c

--- ss14_2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
-int main(){
-    char s[10005];
-    gets(s);
-    for(int i=0;i<strlen(s);i++)printf("%c ", s[i]);
-}
